512c27e38eedd553604c14779c53f89fe85abf8c
mcgodamn <t7214948ttt@gmail.com>
2019-05-27 02:00:51 +0800

    update

---
+++ Assets/Editor/buildAB.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CreateAssetBundles
+{
+
+
+    [MenuItem("Assets/Build AssetBundles")]
+    static void BuildAllAssetBundles()
+    {
+        string assetBundleDirectory = "Assets/AssetBundles";
+        if (!Directory.Exists(assetBundleDirectory))
+        {
+            Directory.CreateDirectory(assetBundleDirectory);
+        }
+        BuildPipeline.BuildAssetBundles(assetBundleDirectory, BuildAssetBundleOptions.None, BuildTarget.StandaloneWindows);
+    }
+
+}
---
+++ Assets/Scrpits/Test.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.IO;
+using System.Collections.Generic;
+using UnityEngine;
+using ILRuntime.Runtime.Enviorment;
+
+public class Test : MonoBehaviour
+{
+    AppDomain appdomain;
+
+    void Start()
+    {
+        LoadILRuntime();
+    }
+
+    void LoadILRuntime()
+    {
+        appdomain = new AppDomain();
+
+        AssetBundle bundle = AssetBundle.LoadFromFile(Application.dataPath + "/AssetBundles/hotfix.fuckyeah");
+
+        TextAsset txt = bundle.LoadAsset("dll.bytes") as TextAsset;
+        byte[] dll = txt.bytes;
+        txt = bundle.LoadAsset("pdb.bytes") as TextAsset;
+        byte[] pdb = txt.bytes;
+
+        using (System.IO.MemoryStream fs = new System.IO.MemoryStream(dll))
+        {
+            using (System.IO.MemoryStream p = new System.IO.MemoryStream(pdb))
+            {
+                appdomain.LoadAssembly(fs, p, new Mono.Cecil.Pdb.PdbReaderProvider());
+            }
+        }
+        OnHotFixLoaded();
+    }
+
+
+
+    void OnHotFixLoaded()
+    {
+        appdomain.Invoke("HotFix_Project.InstanceClass", "StaticFunTest", null, null);
+    }
+}
